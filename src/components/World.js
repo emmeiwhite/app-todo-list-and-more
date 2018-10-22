3ee617e566937da65f601be08afbe4f43285b9a3
emmeiwhite <emmeiwhite@gmail.com>
2018-10-22 13:06:10 +0530

    Hello and World components used within root Component App

---
+++ src/components/World.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class World extends Component {
+    state = {  }
+    render() { 
+        return (
+            <h2>WORLD !!!</h2>
+          );
+    }
+}
+ 
+export default World;
